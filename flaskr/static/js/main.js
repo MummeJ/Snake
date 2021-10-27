b6125e04f07d644e1a8715f53eaf6a0e96222b65
Jeremy Mumme <jeremym@completenetwork.com>
2021-10-27 12:44:20 -0500

    Created grid

---
+++ flaskr/static/js/main.js
@@ -0,0 +1,7 @@
+const grid = document.querySelector('.grid');
+for (let i = 1; i < 577; i++) {
+  const div = document.createElement('div')
+  div.className = 'block'
+  div.setAttribute('id', i)
+  grid.appendChild(div);
+}

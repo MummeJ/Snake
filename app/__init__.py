5e347d4a5a9c16917b0fe6fc5fd7d76becec5410
Jeremy Mumme <jeremym@completenetwork.com>
2021-12-16 14:33:06 -0600

    Restructured files

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,9 @@
 import os
-
 from flask import Flask
 
 app = Flask(__name__)
 app.secret_key = 'fNcgi1wF8ZIv4K1g25GW-Q'
-
-from . import routes
+app.debug = True
+import routes
 # db.init_app(app)
 app.register_blueprint(routes.bp)
--- old/flaskr/routes0.py
+++
@@ -1,7 +0,0 @@
-from flask import (Blueprint, flash, g, redirect, render_template, request, session, url_for)
-
-bp = Blueprint('routes', __name__)
-
-@bp.route('/', methods=['POST', 'GET'])
-def index():
-    return render_template('index.html')

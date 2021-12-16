b4b1930cd3bb18f222cfbc70ac6d055b578a7b9c
Jeremy Mumme <jeremym@completenetwork.com>
2021-12-16 15:45:16 -0600

    Reverted commits

--- __init__.py
+++ __init__.py
@@ -1,10 +1,7 @@
-import os
-
 from flask import Flask
+import routes
 
 app = Flask(__name__)
 app.secret_key = 'fNcgi1wF8ZIv4K1g25GW-Q'
 
-from . import routes
-# db.init_app(app)
 app.register_blueprint(routes.bp)

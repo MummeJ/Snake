74aaaaf0625f24598e947c2756e1a93d14dafc88
Jeremy Mumme <jeremym@completenetwork.com>
2021-12-16 15:12:45 -0600

    Restructured files

--- __init__.py
+++ __init__.py
@@ -4,6 +4,6 @@ from flask import Flask
 app = Flask(__name__)
 app.secret_key = 'fNcgi1wF8ZIv4K1g25GW-Q'
 app.debug = True
-from app import routes
+from flaskr import routes
 # db.init_app(app)
 app.register_blueprint(routes.bp)

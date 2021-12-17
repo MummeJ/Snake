6cc81d673a080a8214f326b8c97ff6b90a25111a
Jeremy Mumme <jeremym@completenetwork.com>
2021-12-17 13:24:53 -0600

    First commit on new branch

--- __init__.py
+++ __init__.py
@@ -1,10 +1,11 @@
 
 import os
 from flask import Flask
-import routes
+# from flask_sqlalchemy import SQLAlchemy
+from . import routes
 
 app = Flask(__name__)
-app.secret_key = 'fNcgi1wF8ZIv4K1g25GW-Q'
-
-# db.init_app(app)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'fNcgi1wF8ZIv4K1g25GW-Q'
+# db = SQLAlchemy(app)
 app.register_blueprint(routes.bp)

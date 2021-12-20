3d70b71da305bef3e1ea7f47b027cafade96e0c8
Jeremy Mumme <jeremym@completenetwork.com>
2021-12-20 16:01:43 -0600

    Added new file structure

--- database.py
+++
@@ -1,9 +0,0 @@
-from app import db
-
-class Leaders(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(12), nullable=False )
-    score = db.Column(db.Integer, nullable=False )
-
-    def __repr__(self):
-        return '<Item {}>'.format(self.id)
